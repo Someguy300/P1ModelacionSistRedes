71fb3f2a22ce130b84279fd23c61979b8058dfdb
Someguy300 <pedrobarriosp190@gmail.com>
2022-03-10 12:48:24 -0400

    Menu basico para usar el programa

--- Proyecto1.py
+++ Proyecto1.py
@@ -1,1 +1,56 @@
-print("Hello, World!")
+#main
+print("""Sistema de gestion de viajes Agencia de Viajes Metro Travel
+Antes de gestionar su viaje debemos preguntarle:""")
+
+while True:
+    print("¿Usted posee visa?")
+    print("Presione [y] y [Enter] para Si")
+    print("Presione [n] y [Enter] para No")
+    inputVisa = input()
+    if inputVisa == 'y' or inputVisa == 'n':
+        break
+    else:
+        print("Ingrese un valor valido [y] o [n]")
+
+
+print()
+if inputVisa == 'y':
+    print("Dada su disponibilidad de visa estas sus sus siguientes opciones para su aeropuerto de origen")
+else:
+    print("Dada su no disponibilidad de visa estas sus sus siguientes opciones para su aeropuerto de origen")
+
+#INSERTAR AEROPUERTOS DE ORIGEN E INPUT AQUI
+
+print()
+print("Los aeropuertos disponibles como aeropuertos destino son:")
+
+#INSERTAR AEROPUERTOS DESTINO E INPUT AQUI
+
+print()
+print("""Como desea planear su viaje?
+Escogiendo la ruta mas barata? ó
+Escogiendo la ruta con numero de vuelos minima?""")
+
+
+
+while True:
+    try:
+        print("Presione [1] y [Enter] para ruta mas barata")
+        print("Presione [2] y [Enter] para numero de vuelos minima")
+        inputCriterio = int(input())
+    except Exception:
+        print("Ingrese un valor valido [1] o [2]")
+        continue
+    else:
+        if inputCriterio == 1 or inputCriterio == 2:
+            break
+        else:
+            print("Ingrese un valor valido [1] o [2]")
+
+print()
+if inputVisa == 1:
+    print("Su viaje de acuerdo a la ruta mas barata es el siguiente")
+else:
+    print("Su viaje de acuerdo a la ruta con el minimo numero de vuelos es el siguiente")
+
+#MOSTRAR RUTAS CON COSTO AQUI
