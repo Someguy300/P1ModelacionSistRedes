bc37a0a082cb520f22a3b4b92d4bc3a4d94be43c
Pedro Barrios <51805154+Someguy300@users.noreply.github.com>
2022-03-16 01:28:30 -0400

    Update Proyecto1.py

--- Proyecto1.py
+++ Proyecto1.py
@@ -4,14 +4,7 @@ from sys import maxsize
 from collections import deque
 from collections import defaultdict
 
-# agregamos lado
-
-def add_edge(adj, src, dest, weight):
-    adj[src][dest] = weight
-    adj[dest][src] = weight
-
 # Eliminar nodo para cuando no tenga visa
-
 def delete_node(adj, vertex):
     for neighbor in list(adj[vertex]):
         if neighbor != vertex:
@@ -75,7 +68,7 @@ def print_paths(old_adj, vertices, start, dest, all_cities):
 
     # Pasamos los nombres de las ciudades a una lista vacia para poder recorrerlas 
     adj = []
-    for key, value in old_adj.items():
+    for value in old_adj.graph.items():
         node_list = []
         for neighbor in value:
             node_list.append(all_cities.index(neighbor))
@@ -158,110 +151,101 @@ def print_paths(old_adj, vertices, start, dest, all_cities):
 #              str(shortest_distance[target]) + ', y el camino es ' + str(path))
 
 
-#Class to represent a graph
-class Graph:
-
-    # A utility function to find the
-    # vertex with minimum dist value, from
-    # the set of vertices still in queue
-    def minDistance(self, dist, queue):
-        # Initialize min value and min_index as -1
-        minimum = float("Inf")
-        min_index = -1
-
-        # from the dist array,pick one which
-        # has min value and is till in queue
-        for i in range(len(dist)):
-            if dist[i] < minimum and i in queue:
-                minimum = dist[i]
-                min_index = i
-        return min_index
-
-    # Function to print shortest path
-    # from source to j
-    # using parent array
-
-    def printPath(self, parent, j):
-
-        #Base Case : If j is source
-        if parent[j] == -1:
-            print(j, end=" ")
-            return
-        self.printPath(parent, parent[j])
-        print(j, end=" ")
-
-    # A utility function to print
-    # the constructed distance
-    # array
-
-    def printSolution(self, dist, parent):
-        src = 0
-        print("Vertex \t\tDistance from Source\tPath")
-        for i in range(1, len(dist)):
-            print("\n%d --> %d \t\t%d \t\t\t\t\t" % (src, i, dist[i]), end=" ")
-            self.printPath(parent, i)
+import sys
+ 
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
+        
+    def construct_graph(self, nodes, init_graph):
+        '''
+        This method makes sure that the graph is symmetrical. In other words, if there's a path from node A to B with a value V, there needs to be a path from node B to node A with a value V.
+        '''
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
+        
+        graph.update(init_graph)
+        
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
+                    
+        return graph
+    
+    def get_nodes(self):
+        "Returns the nodes of the graph."
+        return self.nodes
+    
+    def get_outgoing_edges(self, node):
+        "Returns the neighbors of a node."
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+    
+    def value(self, node1, node2):
+        "Returns the value of an edge between two nodes."
+        return self.graph[node1][node2]
 
 
-  # def dijkstra(graph, source, target):
-    def dijkstra(self, graph, src):
+def print_result(previous_nodes, shortest_path, start_node, target_node):
+    path = []
+    node = target_node
+    
+    while node != start_node:
+        path.append(node)
+        node = previous_nodes[node]
+ 
+    # Add the start node manually
+    path.append(start_node)
+    
+    print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
+    print(" -> ".join(reversed(path)))
 
-        row = len(graph)
-        col = len(graph[0])
+def dijkstra_algorithm(graph, start_node):
+    unvisited_nodes = list(graph.get_nodes())
+ 
+    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph   
+    shortest_path = {}
+ 
+    # We'll use this dict to save the shortest known path to a node found so far
+    previous_nodes = {}
+ 
+    # We'll use max_value to initialize the "infinity" value of the unvisited nodes   
+    max_value = sys.maxsize
+    for node in unvisited_nodes:
+        shortest_path[node] = max_value
+    # However, we initialize the starting node's value with 0   
+    shortest_path[start_node] = 0
+    
+    # The algorithm executes until we visit all nodes
+    while unvisited_nodes:
+        # The code block below finds the node with the lowest score
+        current_min_node = None
+        for node in unvisited_nodes: # Iterate over the nodes
+            if current_min_node == None:
+                current_min_node = node
+            elif shortest_path[node] < shortest_path[current_min_node]:
+                current_min_node = node
+                
+        # The code block below retrieves the current node's neighbors and updates their distances
+        neighbors = graph.get_outgoing_edges(current_min_node)
+        for neighbor in neighbors:
+            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
+            if tentative_value < shortest_path[neighbor]:
+                shortest_path[neighbor] = tentative_value
+                # We also update the best path to the current node
+                previous_nodes[neighbor] = current_min_node
+ 
+        # After visiting its neighbors, we mark the node as "visited"
+        unvisited_nodes.remove(current_min_node)
+    
+    return previous_nodes, shortest_path
 
-        # The output array. dist[i] will hold
-        # the shortest distance from src to i
-        # Initialize all distances as INFINITE
-        dist = [float("Inf")] * row
-
-        #Parent array to store
-        # shortest path tree
-        parent = [-1] * row
-
-        # Distance of source vertex
-        # from itself is always 0
-        dist[src] = 0
-
-        # Add all vertices in queue
-        queue = []
-        for i in range(row):
-            queue.append(i)
-
-        #Find shortest path for all vertices
-        while queue:
-
-            # Pick the minimum dist vertex
-            # from the set of vertices
-            # still in queue
-            u = self.minDistance(dist, queue)
-
-            # remove min element
-            queue.remove(u)
-
-            # Update dist value and parent
-            # index of the adjacent vertices of
-            # the picked vertex. Consider only
-            # those vertices which are still in
-            # queue
-            for i in range(col):
-                '''Update dist[i] only if it is in queue, there is
-                an edge from u to i, and total weight of path from
-                src to i through u is smaller than current value of
-                dist[i]'''
-                if graph[u][i] and i in queue:
-                    if dist[u] + graph[u][i] < dist[i]:
-                        dist[i] = dist[u] + graph[u][i]
-                        parent[i] = u
-
-        # print the constructed distance array
-        self.printSolution(dist, parent)
-
-
-g = Graph()
-
-
-
-# Print the solution
-#g.dijkstra(graph, 0)
 
 
 # main
@@ -269,35 +253,36 @@ g = Graph()
 # variables
 all_cities = ["CCS", "AUA", "BON", "CUR", "SXM",
               "SDQ", "SBH", "POS", "BGI", "FDF", "PTP"]
-places = all_cities
 need_visa_cities = ["AUA", "BON", "CUR", "SXM", "SDQ"]
 vertices = len(all_cities)
 index = 0
 
-# we create the adjacency list
-adj = collections.defaultdict(dict)
 
+init_graph = {}
+for node in all_cities:
+    init_graph[node] = {}
 # we create the graph
-add_edge(adj, "CCS", "AUA", 40)
-add_edge(adj, "CCS", "CUR", 35)
-add_edge(adj, "CCS", "BON", 60)
-add_edge(adj, "CCS", "SDQ", 180)
-add_edge(adj, "CCS", "POS", 150)
-add_edge(adj, "CCS", "BGI", 180)
-add_edge(adj, "AUA", "CUR", 15)
-add_edge(adj, "AUA", "BON", 15)
-add_edge(adj, "CUR", "BON", 15)
-add_edge(adj, "SDQ", "SXM", 50)
-add_edge(adj, "SXM", "SBH", 45)
-add_edge(adj, "POS", "BGI", 35)
-add_edge(adj, "POS", "SXM", 90)
-add_edge(adj, "POS", "PTP", 80)
-add_edge(adj, "POS", "FDF", 75)
-add_edge(adj, "BGI", "SXM", 70)
-add_edge(adj, "PTP", "SXM", 100)
-add_edge(adj, "PTP", "SBH", 80)
-add_edge(adj, "CUR", "SXM", 80)
-add_edge(adj, "AUA", "SXM", 85)
+init_graph["CCS"]["AUA"]=40
+init_graph["CCS"]["CUR"]=35
+init_graph["CCS"]["BON"]=60
+init_graph["CCS"]["SDQ"]=180
+init_graph["CCS"]["POS"]=150
+init_graph["CCS"]["BGI"]=180
+init_graph["AUA"]["CUR"]=15
+init_graph["AUA"]["BON"]=15
+init_graph["CUR"]["BON"]=15
+init_graph["SDQ"]["SXM"]=50
+init_graph["SXM"]["SBH"] =45
+init_graph["POS"]["BGI"]=35
+init_graph["POS"]["SXM"] =90
+init_graph["POS"]["PTP"] =80
+init_graph["POS"]["FDF"] =75
+init_graph["BGI"]["SXM"]= 70
+init_graph["PTP"]["SXM"]= 100
+init_graph["PTP"]["SBH"]= 80
+init_graph["CUR"]["SXM"]= 80
+init_graph["AUA"]["SXM"]= 85
+graph = Graph(all_cities, init_graph)
 
 # we ask if the person has a visa
 print("Bienvenido a la Agencias de Viajes Metro Travel.")
@@ -327,7 +312,7 @@ while True:
 # if he doesn't have a visa, delete the cities that need visa from the graph
 if has_visa == 2:
     for city_visa in need_visa_cities:
-        delete_node(adj, city_visa)
+        delete_node(graph, city_visa)
         all_cities.remove(city_visa)
 
 # first, we ask the origin and destiny
@@ -390,9 +375,10 @@ while True:
 if has_visa == 2 and dest in need_visa_cities:
     print("No posee la visa requerida para entrar")
 elif route_type == 1:
-    dijkstra(adj, src)
+    previous_nodes, shortest_path = dijkstra_algorithm(graph=graph, start_node=src)
+    print_result(previous_nodes, shortest_path, start_node=src, target_node=dest)
 else:
-    print_paths(adj, vertices, all_cities.index(
+    print_paths(graph, vertices, all_cities.index(
         src), all_cities.index(dest), all_cities)
 
 
@@ -400,58 +386,3 @@ else:
 
 
 
-print("""Sistema de gestion de viajes Agencia de Viajes Metro Travel
-Antes de gestionar su viaje debemos preguntarle:""")
-
-while True:
-    print("¿Usted posee visa?")
-    print("Presione [y] y [Enter] para Si")
-    print("Presione [n] y [Enter] para No")
-    inputVisa = input()
-    if inputVisa == 'y' or inputVisa == 'n':
-        break
-    else:
-        print("Ingrese un valor valido [y] o [n]")
-
-
-print()
-if inputVisa == 'y':
-    print("Dada su disponibilidad de visa estas sus sus siguientes opciones para su aeropuerto de origen")
-else:
-    print("Dada su no disponibilidad de visa estas sus sus siguientes opciones para su aeropuerto de origen")
-
-#INSERTAR AEROPUERTOS DE ORIGEN E INPUT AQUI
-
-print()
-print("Los aeropuertos disponibles como aeropuertos destino son:")
-
-#INSERTAR AEROPUERTOS DESTINO E INPUT AQUI
-
-print()
-print("""Como desea planear su viaje?
-Escogiendo la ruta mas barata? ó
-Escogiendo la ruta con numero de vuelos minima?""")
-
-
-
-while True:
-    try:
-        print("Presione [1] y [Enter] para ruta mas barata")
-        print("Presione [2] y [Enter] para numero de vuelos minima")
-        inputCriterio = int(input())
-    except Exception:
-        print("Ingrese un valor valido [1] o [2]")
-        continue
-    else:
-        if inputCriterio == 1 or inputCriterio == 2:
-            break
-        else:
-            print("Ingrese un valor valido [1] o [2]")
-
-print()
-if inputVisa == 1:
-    print("Su viaje de acuerdo a la ruta mas barata es el siguiente")
-else:
-    print("Su viaje de acuerdo a la ruta con el minimo numero de vuelos es el siguiente")
-
-#MOSTRAR RUTAS CON COSTO AQUI
